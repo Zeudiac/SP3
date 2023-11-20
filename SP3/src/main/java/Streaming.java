cfeb10a22a2a517c0824ca9c53d00984bfa4c9b7
Malte <malte.pavon.olesen@hotmail.com>
2023-11-20 17:29:24 +0100

    Streaming updates

--- SP3/src/main/java/Streaming.java
+++ SP3/src/main/java/Streaming.java
@@ -15,7 +15,34 @@ public class Streaming {
         movies=io.readMovieData("MediaFiles/100bedstefilm.txt");
         System.out.println(movies);
 
+    }
+    public void runStreaming(){
+        protected Map<String,String> userCredentials;// En instansvariabel, der gemmer brugernavne og adgangskoder.
+        String path;
 
+        public void userAndPassoword(){
+
+            // Opretter en ny HashMap til at gemme brugernavne og adgangskoder.
+            userCredentials= new HashMap<String,String>();
+
+            // Opretter en ny instans af TextUI-klassen.
+            TextUI ui = new TextUI();
+            String input="";
+
+
+            // Bruger TextUI-klassen til at indhente brugernavnet fra brugeren.
+            input=ui.getInput("Enter username: ");
+            String username= input;
+
+            input= ui.getInput("Enter passoword: ");
+            String password=input;
+
+            // TilfÃ¸jer brugernavnet og adgangskoden til HashMap.
+            userCredentials.put(username,password);
+            // Gemmer brugeroplysninger i filen "Accounts.txt"
+            FileIO.saveUserData(userCredentials,"/Users/alfredofernandez/Desktop/Datamatiker/SP3/SP3/Accounts/Accounts.txt");
+
+        }
     }
 
 }
